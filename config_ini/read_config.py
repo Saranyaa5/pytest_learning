70bc0939821eef6208d6cf792d63369ec216e426
SARANYA ALAGESAN <114178067+Saranyaa5@users.noreply.github.com>
2025-05-13 22:39:23 +0530

    Update read_config.py

--- config_ini/read_config.py
+++ config_ini/read_config.py
@@ -1,6 +1,16 @@
-from configparser import ConfigParser
+# from configparser import ConfigParser
 
-def get_config(category,key):
-    config=ConfigParser()
-    config.read("./config.ini")
-    return config.get(category,key)
+# def get_config(category,key):
+#     config=ConfigParser()
+#     config.read("./config.ini")
+#     return config.get(category,key)
+
+from configparser import ConfigParser
+import os
+
+def get_config(category, key):
+    config = ConfigParser()
+    # Get path relative to this script's location
+    config_path = os.path.join(os.path.dirname(__file__), "config.ini")
+    config.read(config_path)
+    return config.get(category, key)
