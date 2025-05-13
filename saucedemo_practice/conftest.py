0d5f02a01b168366dc688dca48c87717833db587
Saranyaa5 <2k21cse128@kiot.ac.in>
2025-05-14 00:10:48 +0530

    updated saucedemo practice module

--- saucedemo_practice/conftest.py
+++ saucedemo_practice/conftest.py
@@ -1,17 +1,25 @@
 from selenium import webdriver
 import pytest
 from utilities.config_reader import get_config
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.edge.options import Options as EdgeOptions
 
 @pytest.fixture()
 def setup_and_tear_down(request):
-    url=get_config("config info","url")
-    browser=get_config("config info","browser")
-    if browser=="chrome":
-        driver=webdriver.Chrome()
-    elif browser=="edge":
-        driver=webdriver.Edge()
+    url = get_config("config info", "url")
+    browser = get_config("config info", "browser")
+    if browser == "chrome":
+        # Set up Chrome options for incognito mode
+        chrome_options = Options()
+        chrome_options.add_argument("--incognito")
+        driver = webdriver.Chrome(options=chrome_options)
+    elif browser == "edge":
+        # Set up Edge options for incognito mode
+        edge_options = EdgeOptions()
+        edge_options.add_argument("--inprivate")
+        driver = webdriver.Edge(options=edge_options)
+    driver.maximize_window()
     driver.get(url)
-    request.cls.driver=driver
+    request.cls.driver = driver
     yield
     driver.quit()
-
---
+++ saucedemo_practice/test_module2.py
@@ -0,0 +1,57 @@
+from selenium.webdriver.common.by import By
+import pytest
+from utilities.excel_reader import get_exceldata
+import time
+
+@pytest.mark.usefixtures("setup_and_tear_down")
+class TestLogin2:
+
+    # Retrieve the login data manually from the Excel sheet
+    login_data = get_exceldata("C:\pytest_learning\saucedemo_practice\excelfile\login_input.xlsx", "Sheet1")
+
+    def test_login_locked_out_user(self):
+        # Test for the first user in the login_data
+        username, password = self.login_data[0]
+        self.driver.find_element(By.ID, "user-name").send_keys(username)
+        self.driver.find_element(By.ID, "password").send_keys(password)
+        self.driver.find_element(By.ID, "login-button").click()
+
+        if username == "locked_out_user":
+            time.sleep(2)
+            assert self.driver.find_element(By.XPATH, "//*[@id='login_button_container']/div/form/h3").is_displayed()
+
+    def test_login_standard_user(self):
+        # Test for the second user in the login_data
+        username, password = self.login_data[1]
+        self.driver.find_element(By.ID, "user-name").send_keys(username)
+        self.driver.find_element(By.ID, "password").send_keys(password)
+        self.driver.find_element(By.ID, "login-button").click()
+
+        if username != "locked_out_user":
+            time.sleep(2)
+            self.driver.find_element(By.XPATH, "//*[@id='menu_button_container']/div/div[3]/div").click()
+            time.sleep(2)
+            assert self.driver.find_element(By.XPATH, "//*[@id='logout_sidebar_link']").is_displayed()
+
+    def test_login_problem_user(self):
+        # Test for the third user in the login_data
+        username, password = self.login_data[2]
+        self.driver.find_element(By.ID, "user-name").send_keys(username)
+        self.driver.find_element(By.ID, "password").send_keys(password)
+        self.driver.find_element(By.ID, "login-button").click()
+
+        if username == "locked_out_user":
+            time.sleep(2)
+            assert self.driver.find_element(By.XPATH, "//*[@id='login_button_container']/div/form/h3").is_displayed()
+
+    def test_login_performance_glitch_user(self):
+        # Test for the fourth user in the login_data
+        username, password = self.login_data[3]
+        self.driver.find_element(By.ID, "user-name").send_keys(username)
+        self.driver.find_element(By.ID, "password").send_keys(password)
+        self.driver.find_element(By.ID, "login-button").click()
+
+        if username == "locked_out_user":
+            time.sleep(2)
+            assert self.driver.find_element(By.XPATH, "//*[@id='login_button_container']/div/form/h3").is_displayed()
+
